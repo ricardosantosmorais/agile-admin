6e6b61bd70f59938dd2c9c614cc431e84f132fdc
Ricardo Morais <ricardo.morais@gmail.com>
2026-04-23 19:37:30 -0300

    Reduce Amplify build output size

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,22 @@
-import type { NextConfig } from 'next'
-import { withSentryConfig } from '@sentry/nextjs'
+import type { NextConfig } from 'next';
+import { withSentryConfig } from '@sentry/nextjs';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+};
 
 export default withSentryConfig(nextConfig, {
-  org: 'agileecommerce-b2b',
-  project: 'javascript-nextjs',
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-  silent: !process.env.CI,
-  webpack: {
-    treeshake: {
-      removeDebugLogging: true,
-    },
-  },
-  sourcemaps: {
-    disable: !process.env.SENTRY_AUTH_TOKEN,
-  },
-})
+	org: 'agileecommerce-b2b',
+	project: 'javascript-nextjs',
+	authToken: process.env.SENTRY_AUTH_TOKEN,
+	silent: !process.env.CI,
+	webpack: {
+		treeshake: {
+			removeDebugLogging: true,
+		},
+	},
+	sourcemaps: {
+		disable: !process.env.SENTRY_AUTH_TOKEN,
+		deleteSourcemapsAfterUpload: true,
+	},
+});
